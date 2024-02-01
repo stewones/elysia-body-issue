6f0b4e13b608b0d3a77a45b0cb9beeb0e80db450
stewones <oi@stewan.io>
2024-01-31 22:12:15 -0300

    add body issue reproduction

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app2 = new Elysia().post("/", ({ body }) => body);
+
+const app1 = new Elysia()
+  .mount("/app2", app2)
+  .post("/", ({ body }) => body)
+  .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app1.server?.hostname}:${app1.server?.port}`
 );
